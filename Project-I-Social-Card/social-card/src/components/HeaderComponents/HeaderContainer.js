c8f2fb6ea3638fc1566c48f12b304061215e7d17
Lambda_School_Loaner_114 <lambda_school_loaner_114@Lambda-School-Loaner-114s-MacBook-Pro.local>
2019-05-28 17:25:08 -0700

    Created directories in src/components and updated header files

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from './HeaderContent';
+
+const HeaderContainer = () => {
+    return (
+        <div className="HeaderContainer">
+            <ImageThumbnail />
+            <HeaderContent />
+        </div>
+    );
+};
+
+export default HeaderContent;
