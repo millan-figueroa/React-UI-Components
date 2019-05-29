4406cd57e852fd2d14832c2039b14a860f2ffeec
Millan Figueroa <lambda_school_loaner_114@Lambda-School-Loaner-114s-MacBook-Pro.local>
2019-05-28 18:09:57 -0700

    Added files to app.js, added some styles

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="HeaderTitle">
+            <h1>Lambda School</h1>
+            <h3>@LambdaSchool</h3>
+            <h3>.</h3>
+            <h3>May 24</h3>
+        </div>
+    );
+};
+
+export default HeaderTitle;
